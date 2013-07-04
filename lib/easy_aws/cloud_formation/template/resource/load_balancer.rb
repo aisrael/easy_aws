40cb3097d0cce6364cc3636f97989249802f1cf2
Alistair A. Israel <aisrael@gmail.com>
2013-07-04 11:26:42 +0800

    Um. More stuff. Not really sure which method_missing gets called now LOL

--- lib/easy_aws/cloud_formation/template/resource/load_balancer.rb
+++ lib/easy_aws/cloud_formation/template/resource/load_balancer.rb
@@ -1,31 +1,39 @@
-class EasyAWS::CloudFormation::Template::Resource
-  class LoadBalancer < EasyAWS::CloudFormation::Template::Resource
-    def listeners
-      properties[:listeners] ||= []
-    end
+module EasyAWS
+  module CloudFormation
+    class Template
+      class Resource
+        class LoadBalancer < Resource
+          def listeners
+            properties[:listeners] ||= []
+          end
 
-    def listener(lb_protocol, lb_port, instance_protocol, instance_port, certificate_id = nil)
-      hash = {
-        'Protocol' => lb_protocol,
-        'LoadBalancerPort' => lb_port,
-        'InstanceProtocol' => instance_protocol,
-        'InstancePort' => instance_port
-      }
-      hash['SSLCertificateId'] = certificate_id unless certificate_id.blank?
-      listeners << hash
-    end
+          def listener(lb_protocol, lb_port, instance_protocol, instance_port, certificate_id = nil)
+            hash = {
+              'Protocol' => lb_protocol,
+              'LoadBalancerPort' => lb_port,
+              'InstanceProtocol' => instance_protocol,
+              'InstancePort' => instance_port
+            }
+            hash['SSLCertificateId'] = certificate_id unless certificate_id.blank?
+            listeners << hash
+          end
 
-    def availability_zones(*zones)
-      if properties.has_key?(:availability_zones)
-        properties[:availability_zones].concat!(zones)
-      else
-        properties.store(:availability_zones, zones)
-      end
-    end
+          def availability_zones(*zones)
+            if properties.has_key?(:availability_zones)
+              properties[:availability_zones].concat!(zones)
+            else
+              properties.store(:availability_zones, zones)
+            end
+          end
 
-    def method_missing(method_name, *args)
-      if args && args.size == 1
-        properties.store(method_name, args.first)
+          def method_missing(method_name, *args)
+            if args && args.size == 1
+              properties.store(method_name, args.first)
+            else
+              super
+            end
+          end
+        end
       end
     end
   end
